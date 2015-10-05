d11eb6ce0a5e810a9c55f9b0a46defe12607b02d
zerozakiark <zerozakiark@gmail.com>
2015-10-05 14:22:44 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,41 +4,42 @@ import javax.swing.*;
 
 public class Calculator extends JFrame implements ActionListener{
 	
-	//Main method
-	public static void main(String[] args) {
-	   Calculator calculator = new Calculator();// create calculator
-	   calculator.setTitle("Calculator"); // call it "calculator"
-	   calculator.setSize(400, 400); // size 400 * 400
-	   calculator.setLocationRelativeTo(null); // Center the frame 
-	   calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	   calculator.setVisible(true);
-	}
+  //Main method
+  public static void main(String[] args) {
+     Calculator calculator = new Calculator();// create calculator
+     calculator.setTitle("Calculator"); // call it "calculator"
+     calculator.setSize(400, 400); // size 400 * 400
+     calculator.setLocationRelativeTo(null); // Center the frame 
+     calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     calculator.setVisible(true);
+  }
 	
-	
-	JTextField number_field = new JTextField("0");
-	String number_string = "";
-	double number;
-	char jbutton[] = {'7','8','9','/','4','5','6','*','1','2','3','-','0','.','=','+'};
-	JButton button[] = new JButton[16];
-	char operator;
-	boolean point;
+  //
+  JTextField number_field = new JTextField("0");
+  String number_string = "";
+  double number;
+  char jbutton[] = {'7','8','9','/','4','5','6','*','1','2','3','-','0','.','=','+'};
+  JButton button[] = new JButton[16];
+  char operator;
+  boolean point;
   
-	public Calculator(){
-	   JPanel panel = new JPanel();
-	   Container container = getContentPane();
-	   container.setLayout(new BorderLayout());
-    	   panel.setLayout(new GridLayout(4,4));
-	   number_field.setHorizontalAlignment(JTextField.RIGHT);
-		
-	   for(int i=0;i<16;i++){
-              button[i]=new JButton(jbutton[i]+"");
-              panel.add(button[i]);
-              button[i].addActionListener(this);
-           }
-	   container.add("North",number_field);
-           container.add("Center",panel);
-	}
+  //
+  public Calculator(){
+     JPanel panel = new JPanel();
+     Container container = getContentPane();
+     container.setLayout(new BorderLayout());
+     panel.setLayout(new GridLayout(4,4));
+     number_field.setHorizontalAlignment(JTextField.RIGHT);
+     for(int i=0;i<16;i++){
+        button[i]=new JButton(jbutton[i]+"");
+        panel.add(button[i]);
+        button[i].addActionListener(this);
+     }
+     container.add("North",number_field);
+     container.add("Center",panel);
+  }
 	
+  //	
   public void actionPerformed(ActionEvent event){   
      if(((JButton)event.getSource()).getText().charAt(0)=='.'){
         if(!point){
@@ -56,26 +57,24 @@ public class Calculator extends JFrame implements ActionListener{
         number_string="";
         number_field.setText(operation(number,Double.parseDouble(number_field.getText()),operator)+"");
         operator = ((JButton)event.getSource()).getActionCommand().charAt(0);
-            
         if(((JButton)event.getSource()).getText().charAt(0)!='=')                  
             number=Double.parseDouble(number_field.getText());               
      }         
  }
  
-    // method of choosing " + " , " - " , " * " and " / " , and return their answer
-    public double operation(double number1,double number2,char operator){      
-        switch(operator)
-        {        
-            case '+':
-                return (number1 + number2);
-            case '-':
-                return (number1 - number2);
-            case '*':
-                return (number1 * number2);
-            case '/':
-                return (number1 / number2);
-            default:
-                return number2; 
-        }
-    } 
+  // method of choosing " + " , " - " , " * " and " / " , and return their answer
+  public double operation(double number1,double number2,char operator){      
+     switch(operator){        
+        case '+':
+           return (number1 + number2);
+        case '-':
+           return (number1 - number2);
+        case '*':
+           return (number1 * number2);
+        case '/':
+           return (number1 / number2);
+        default:
+           return number2; 
+     }
+  } 
 }
